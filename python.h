129948cb57db0bd22ddea6692ab859af22961754
Andrew <andriycherpak.education@gmail.com>
2021-02-24 16:38:35 +0200

    all bugs from last commit fixed; python.cpp migrate to python.h and had been deleted

--- python.h
+++ python.h
@@ -9,11 +9,63 @@
 #include <stdio.h>
 #include <iomanip>
 
-std::vector<std::string> split(std::string, char);
-std::string input(std::string);
+std::vector<std::string> split(std::string line, char sep = ' ') {
+	std::vector<std::string> res;
+	std::string slice = "";
+	line += sep;
+	for (int i = 0; i < int(line.length()); i++) {
+		if (line[i] == sep) {
+			if (slice.length() > 0) res.push_back(slice);
+			slice = "";
+		}
+		else slice += line[i];
+	}
+	return res;
+}
+
+std::string input(std::string message = "") {
+	std::string res;
+	std::cout << message; getline(std::cin, res); std::cout << std::endl;
+	return res;
+}
+
+
+std::vector<int> range(int start, int finish, int step = 1) {
+	std::vector<int> res;
+	for (int i = start; i < finish; i += step) {
+		res.push_back(i);
+	}
+	return res;
+}
 
 template <typename T>
-void sort(T*, size_t, bool);
+void sort(T* arr, size_t n, bool reverse=false) {
+	for (int i = 0; i < int(n); i++) {
+		for (int j = 0; j < int(n - 1 - i); j++) {
+			if (arr[j] > arr[j + 1]) std::swap(arr[j], arr[j + 1]);
+		}
+	}
+	if (reverse) {
+		for (int i = 0; i < int(n)/2; i++) {
+			std::swap(arr[i], arr[n - i - 1]);
+		}
+	}
+}
 
-std::vector<int> range(int, int, int);
-void print(int*, size_t);
+template <typename T>
+void print(std::vector<T> v) {
+	for (T a : v) {
+		std::cout << a;
+		if (a != v[v.size() - 1]) std::cout << ", ";
+	}
+	std::cout << std::endl;
+}
+
+void print(int* arr, size_t n) {
+	for (int i = 0; i < int(n); i++) {
+		std::cout << arr[i];
+		if (i != n - 1) std::cout << ", ";
+	}
+	std::cout << std::endl;
+}
+
